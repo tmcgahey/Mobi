e9df81191c2672d512c0871ceecb34ea53e58004
tmcgahey <travis.mcgahey@gmail.com>
2010-10-15 07:46:48 -0400

    Can add items to the summary list

--- IOU/src/com/mobi/iou/server/Account.java
+++ IOU/src/com/mobi/iou/server/Account.java
@@ -1,5 +1,6 @@
 package com.mobi.iou.server;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -7,6 +8,7 @@ import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
+
 
 import com.google.appengine.api.datastore.Key;
 
@@ -19,13 +21,15 @@ public class Account {
 	
 	private String accountName;
 	private String accountOwnerID;
+	private double accountSum = 0.0;
 	
 	@Persistent(mappedBy = "account")
-	private List<AccountLineItem> lineItems;
+	private List<AccountLineItem> lineItems = null;
 	
 	public Account(String accountName, String accountOwnerID) {
 		this.accountName = accountName;
 		this.accountOwnerID = accountOwnerID;
+		accountSum = 0.0;
 	}
 	
 	public Key getKey() {
@@ -53,8 +57,22 @@ public class Account {
 	}
 	
 	public void addLineItem(String description,double amount, Date transactionDate ) {
+		accountSum += amount;
 		AccountLineItem lineItem = new AccountLineItem(description, amount,transactionDate);
+		
+		if(lineItems == null) {
+			lineItems = new ArrayList<AccountLineItem>();
+		}
+		
 		lineItems.add(lineItem);
+	}
+
+	public void setAccountSum(double accountSum) {
+		this.accountSum = accountSum;
+	}
+
+	public double getAccountSum() {
+		return accountSum;
 	}
 
 	
--- IOU/src/com/mobi/iou/server/IOUSummaryServiceImpl.java
+++ IOU/src/com/mobi/iou/server/IOUSummaryServiceImpl.java
@@ -29,11 +29,11 @@ public class IOUSummaryServiceImpl extends RemoteServiceServlet implements IOUSu
 	public ArrayList<SummaryDetails> AddItemReturnSummary(String name,String description, double amount, Date transactionDate) {
 		UserService userService = UserServiceFactory.getUserService();
 		User user = userService.getCurrentUser();
-		
-		Account addToAccount = CreateOrRetrieveAccount(name, user.getUserId());
-		addToAccount.addLineItem(description, amount, transactionDate);
-        
 		PersistenceManager pm = PMF.get().getPersistenceManager();
+		
+		Account addToAccount = CreateOrRetrieveAccount(name, user.getUserId(),pm);
+		addToAccount.addLineItem(description, amount, transactionDate);
+		
 		Transaction tx = pm.currentTransaction();
 		
 		try {
@@ -44,33 +44,32 @@ public class IOUSummaryServiceImpl extends RemoteServiceServlet implements IOUSu
 			if (tx.isActive()) {
 				tx.rollback();
 			}
+			pm.close();
 		}
 		
         return getSummary(user.getUserId());
 	}
 	
 	@SuppressWarnings("unchecked")
-	private Account CreateOrRetrieveAccount(String accountName, String userID) {
+	private Account CreateOrRetrieveAccount(String accountName, String userID, PersistenceManager pm) {
 		
-		PersistenceManager pm = PMF.get().getPersistenceManager();
 		Query query = pm.newQuery(Account.class);
 		query.setFilter("accountName == accountNameParam && accountOwnerID == accountOwnerIDParam");
 		query.declareParameters("String accountNameParam, String accountOwnerIDParam");
 		
 		List<Account> results;
+		Account currentAccount = null;
 		
 		try {
 			 results = (List<Account>) query.execute(accountName,userID);
+			 if(results.size() == 1 ) {
+				 currentAccount = results.get(0);
+			 } else {
+				 currentAccount = new Account(accountName, userID);
+				 //pm.makePersistent(currentAccount);
+			 }
 		} finally {
 			query.closeAll();
-		}
-		
-		Account currentAccount = null;
-		
-		if(results.size() == 1 ) {
-			currentAccount = results.get(0);
-		} else {
-			currentAccount = new Account(accountName, userID);
 		}
 		
 		return currentAccount;
@@ -78,22 +77,35 @@ public class IOUSummaryServiceImpl extends RemoteServiceServlet implements IOUSu
 	}
 	
 	
+	@SuppressWarnings("unchecked")
 	private ArrayList<SummaryDetails> getSummary(String userID) {
 		ArrayList<SummaryDetails> summaryList = new ArrayList<SummaryDetails>();
 		
-		/*PersistenceManager pm = PMF.get().getPersistenceManager();
+		PersistenceManager pm = PMF.get().getPersistenceManager();
 		Query query = pm.newQuery(Account.class);
+		query.setFilter("accountOwnerID == accountOwnerIDParam");
+		query.declareParameters("String accountOwnerIDParam");
+
+		List<Account> accounts = null;
+		try {
+			accounts = (List<Account>) query.execute(userID);
+		} finally {
+			query.closeAll();
+		}
 		
-	    String query = "select from " + AccountLineItem.class.getName();
-	    List<AccountLineItem> lineItems = (List<AccountLineItem>) pm.newQuery(query).execute();
-	    
-	    if (!lineItems.isEmpty()) {
-	    	for (AccountLineItem li : lineItems) {
-	    		summaryList.add(new SummaryDetails(li.getName(), li.getDescription(), li.getAmount()));
-	    	}
-	    }
-		
-	    pm.close();*/
+		if (!accounts.isEmpty()) {
+			for (Account a : accounts) {				
+				
+				summaryList.add(new SummaryDetails(a.getAccountName(), "", a.getAccountSum()));
+				
+				/*if(!a.getLineItems().isEmpty()) {
+					
+					for (AccountLineItem li : a.getLineItems()) {
+			    		summaryList.add(new SummaryDetails(li.getAccount().getAccountName(), li.getDescription(), li.getAmount()));
+			    	}
+				}*/
+			}
+		}
 	    
 		return summaryList;
 		
